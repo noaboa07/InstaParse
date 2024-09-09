752956a5d8cdf6a50fe754a6738f95d262b896d4
noaboa07 <143129234+noaboa07@users.noreply.github.com>
2024-09-09 15:08:55 -0600

    Step 1 Complete
    User can now sign up and data will be stored in Back4App

--- lab-insta-parse/lab-insta-parse/Login/SignUpViewController.swift
+++ lab-insta-parse/lab-insta-parse/Login/SignUpViewController.swift
@@ -2,12 +2,10 @@
 //  SignUpViewController.swift
 //  lab-insta-parse
 //
-//  Created by Charlie Hieger on 11/1/22.
+//  Created by Noah Russell on 9/9/24.
 //
 
 import UIKit
-
-// TODO: Pt 1 - Import Parse Swift
 import ParseSwift
 
 class SignUpViewController: UIViewController {
@@ -22,7 +20,7 @@ class SignUpViewController: UIViewController {
     }
 
     @IBAction func onSignUpTapped(_ sender: Any) {
-
+        
         // Make sure all fields are non-nil and non-empty.
         guard let username = usernameField.text,
               let email = emailField.text,
@@ -30,13 +28,31 @@ class SignUpViewController: UIViewController {
               !username.isEmpty,
               !email.isEmpty,
               !password.isEmpty else {
-
+            
             showMissingFieldsAlert()
             return
         }
+        
+        var newUser = User()
+        newUser.username = username
+        newUser.email = email
+        newUser.password = password
 
-        // TODO: Pt 1 - Parse user sign up
+        newUser.signup { [weak self] result in
 
+            switch result {
+            case .success(let user):
+
+                print("✅ Successfully signed up user \(user)")
+
+                // Post a notification that the user has successfully signed up.
+                NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
+
+            case .failure(let error):
+                // Failed sign up
+                self?.showAlert(description: error.localizedDescription)
+            }
+        }
     }
 
     private func showAlert(description: String?) {
